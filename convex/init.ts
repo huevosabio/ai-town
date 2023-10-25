8c44f0e0f402b3f3172f88df9824e4470d548fd4
Ramon Iglesias <rdit@stanford.edu>
2023-10-25 14:48:50 -0700

    adds tileset and new map time to see if I can merge with zaranova main

--- convex/init.ts
+++ convex/init.ts
@@ -4,8 +4,9 @@ import { DatabaseReader, MutationCtx, internalMutation, mutation } from './_gene
 import { Descriptions } from '../data/characters';
 //import * as firstmap from '../data/firstmap';
 //import * as firstmap from '../data/mage';
-import * as firstmap from '../data/gentle';
+//import * as firstmap from '../data/gentle';
 //import * as firstmap from '../data/mage';
+import * as firstmap from '../data/mj_test_map';
 import { insertInput } from './game/main';
 import { Doc } from './_generated/dataModel';
 import { createEngine, kickEngine, startEngine, stopEngine } from './engine/game';
